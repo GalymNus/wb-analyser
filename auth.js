6e42850aba38587ceb8737dc151831b9beccc4e5
Galym Nus <justtairex@gmail.com>
2026-04-06 21:16:55 +0500

    feat: enhance user authentication flow with Firestore integration and report management

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { firebaseConfig } from "./config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -9,11 +10,13 @@ import {
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
 import { addContainer } from "./index.js";
 
 let isAuth = true;
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
 const emailInput = document.getElementById("email");
@@ -24,20 +27,71 @@ const calcContainer = document.getElementById("calcContainer");
 const authContainer = document.getElementById("authContainer");
 const authUserInfo = document.getElementById("authUserInfo");
 const authUserLogin = document.getElementById("authUserLogin");
+const historyContainer = document.getElementById("history");
 const authModeLink = document.getElementById("authModeLink");
+const finalResult = document.getElementById("finalResult");
+
 const authModeLabel = document.getElementById("authModeLabel");
 const passwordConfirm = document.getElementById("passwordConfirm");
 const authTitle = document.getElementById("authTitle");
+const authUserTokens = document.getElementById("authUserTokens");
 
-authButton.onclick = () => {
+export const db = getFirestore();
+
+console.log("auth", auth);
+
+async function registerAndAddCredits(email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    console.log("User registered in Firebase Authentication:", user.uid);
+    const userDocRef = doc(db, "users", user.uid); // Reference to the user's document using their UID
+    await setDoc(userDocRef, {
+      email: user.email,
+      credits: 3,
+      createdAt: new Date(),
+    });
+
+    console.log("User document created in Firestore with initial credits for:", user.uid);
+    return user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error("Error during user registration or credit assignment:", errorCode, errorMessage);
+    throw error;
+  }
+}
+
+async function getTokens(userId) {
+  try {
+    const userDocRef = doc(db, "users", userId);
+    const userDocSnap = await getDoc(userDocRef);
+    if (userDocSnap.exists()) {
+      console.log("Document data:", userDocSnap.data());
+      window.tokens = userDocSnap.data().credits;
+      authUserTokens.innerText = `${window.tokens} 🪙`;
+    } else {
+      console.log("No such document!");
+    }
+  } catch (error) {
+    console.error("Error getting user document:", error);
+    throw error;
+  }
+}
+
+authButton.onclick = async () => {
   if (isAuth) {
-    signInWithEmailAndPassword(auth, emailInput.value, passInput.value).catch(
-      (err) => (statusMsg.innerText = "Ошибка: " + err.message),
-    );
+    signInWithEmailAndPassword(auth, emailInput.value, passInput.value)
+      .catch((err) => (statusMsg.innerText = "Ошибка: " + err.message))
+      .then(async () => {
+        await getTokens(auth.currentUser.uid);
+      });
   } else {
-    createUserWithEmailAndPassword(auth, emailInput.value, passInput.value).catch(
-      (err) => (statusMsg.innerText = "Ошибка: " + err.message),
-    );
+    await registerAndAddCredits(emailInput.value, passInput.value)
+      .catch((err) => (statusMsg.innerText = "Ошибка: " + err.message))
+      .then(async () => {
+        await getTokens(auth.currentUser.uid);
+      });
   }
 };
 
@@ -67,9 +121,13 @@ btnLogout.onclick = () => {
 const loginChange = (user) => {
   authUserInfo.style.display = "flex";
   authUserLogin.innerText = user ? user.email : "admin";
+  if (window.tokens) {
+    authUserTokens.innerText = `${window.tokens} 🪙`;
+  }
   authContainer.style.display = "none";
   authButton.style.display = "none";
   emailInput.style.display = "none";
+  historyContainer.innerHTML = `<div class="container shadow-deep"><button onclick="getReports()" /> Получить отчеты </button></div>`;
   passInput.style.display = "none";
   statusMsg.innerText = "";
   addContainer();
@@ -78,8 +136,11 @@ const loginChange = (user) => {
 const logountChange = () => {
   authUserInfo.style.display = "none";
   authUserLogin.innerText = "";
+  authUserTokens.innerText = "";
+  historyContainer.innerHTML = "";
   authContainer.style.display = "block";
   authButton.style.display = "inline-block";
+  finalResult.innerHTML = "";
   emailInput.style.display = "inline-block";
   passInput.style.display = "inline-block";
   statusMsg.innerText = "";
